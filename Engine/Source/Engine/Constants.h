9632823fb554f85b8b795e13a58e373dd3f165e7
polyshade-engine <polyshade.engine@gmail.com>
2026-05-07 15:01:45 -0400

    Bug: Wrong version

--- Engine/Source/Engine/Constants.h
+++ Engine/Source/Engine/Constants.h
@@ -2,7 +2,7 @@
 
 #define ENGINE_NAME "Polyphase"
 #define POLYPHASE_VERSION 6
-#define POLYPHASE_VERSION_STRING "6.1.1-beta.120"
+#define POLYPHASE_VERSION_STRING "6.1.1-beta.20"
 #define MAX_PATH_SIZE 260
 
 #define DEFAULT_GAME_NAME "Polyphase"
